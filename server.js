5a751f7c291ac18ed8253bb156477daf48a5a569
reneang17 <reneang17@gmail.com>
2026-02-02 15:09:16 -0600

    Fix: Update Express wildcard route for v5 compatibility

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(join(__dirname, 'dist')));
 
-app.get('*', (req, res) => {
+app.get('(.*)', (req, res) => {
   res.sendFile(join(__dirname, 'dist', 'index.html'));
 });
 
